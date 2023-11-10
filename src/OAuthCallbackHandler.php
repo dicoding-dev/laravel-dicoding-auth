2331ae90fe997ac7138d5b391dff07e3f3ee6731
Rizqy H <rizqy@dicoding.com>
2023-11-10 16:49:35 +0700

    add abstract method to handle access denied response

--- src/OAuthCallbackHandler.php
+++ src/OAuthCallbackHandler.php
@@ -4,6 +4,7 @@ namespace DicodingDev\LaravelDicodingAuth;
 
 use Carbon\Carbon;
 use Exception;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Laravel\Socialite\Contracts\User;
@@ -11,18 +12,23 @@ use Laravel\Socialite\Facades\Socialite;
 
 trait OAuthCallbackHandler
 {
-    public function handle()
+    public function handle(Request $request)
     {
         try {
             $this->setUserAsAuthed(Socialite::driver('dicoding')->user());
 
             return $this->handleSuccessfulAuth();
         } catch (Exception $exception) {
+            if ($request->query('error') === 'access_denied') {
+                return $this->handleAccessDenied();
+            }
+
             return $this->handleFailedAuth($exception);
         }
     }
 
     abstract protected function handleSuccessfulAuth();
+    abstract protected function handleAccessDenied();
     abstract protected function handleFailedAuth(Exception $exception);
 
     private function setUserAsAuthed(User $dicodingUser): void
